3879da1723da104eeaefaa3ff7deee4fc0da4daf
BLUEBL0B <150084458+BLUEBL0B@users.noreply.github.com>
2024-10-06 16:53:09 +0300

    WARP

--- install-server.sh
+++ install-server.sh
@@ -880,6 +880,8 @@ install_packages() {
     curl -fsSL https://pkg.cloudflareclient.com/pubkey.gpg | gpg --yes --dearmor --output /usr/share/keyrings/cloudflare-warp-archive-keyring.gpg
     echo "deb [signed-by=/usr/share/keyrings/cloudflare-warp-archive-keyring.gpg] https://pkg.cloudflareclient.com/ $(grep "VERSION_CODENAME=" /etc/os-release | cut -d "=" -f 2) main" | tee /etc/apt/sources.list.d/cloudflare-client.list
     apt-get update && apt-get install cloudflare-warp -y
+    wget https://pkg.cloudflareclient.com/pool/$(grep "VERSION_CODENAME=" /etc/os-release | cut -d "=" -f 2)/main/c/cloudflare-warp/cloudflare-warp_2024.6.497-1_amd64.deb
+    dpkg -i cloudflare-warp_2024.6.497-1_amd64.deb
 
     if [ ! -d /etc/apt/keyrings ]
     then
