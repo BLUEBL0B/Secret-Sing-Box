61bf736011c0908627431b13f8cf22b3d3326d43
BLUEBL0B <150084458+BLUEBL0B@users.noreply.github.com>
2025-01-24 14:36:48 +0300

    Update

--- Scripts/sb-pc-linux-en.sh
+++ Scripts/sb-pc-linux-en.sh
@@ -1,1 +1,231 @@
+#!/bin/bash
 
+textcolor='\033[1;34m'
+red='\033[1;31m'
+clear='\033[0m'
+
+check_root() {
+	if [[ $EUID -ne 0 ]]
+	then
+		echo ""
+		echo -e "${red}Error: this script should be run as root, use \"sudo -i\" command${clear}"
+		echo ""
+		exit 1
+	fi
+}
+
+install_sing_box() {
+	if [[ ! -f /usr/local/bin/proxylist ]]
+	then
+		touch /usr/local/bin/proxylist
+	fi
+
+	if [ $(sing-box version &> /dev/null; echo $?) -ne 0 ]
+	then
+		echo ""
+		echo -e "${textcolor}Sing-Box is not installed${clear}"
+		echo ""
+		echo -e "${textcolor}[?]${clear} Press ${textcolor}Enter${clear} to install it or enter ${textcolor}x${clear} to exit:"
+		read sbinstall
+		exit_install
+
+		echo -e "${textcolor}Installing Sing-Box...${clear}"
+		curl -fsSL https://sing-box.app/gpg.key -o /etc/apt/keyrings/sagernet.asc
+		chmod a+r /etc/apt/keyrings/sagernet.asc
+		echo "deb [arch=`dpkg --print-architecture` signed-by=/etc/apt/keyrings/sagernet.asc] https://deb.sagernet.org/ * *" | tee /etc/apt/sources.list.d/sagernet.list > /dev/null
+		apt-get update
+		apt-get install sing-box -y
+		systemctl disable sing-box.service
+
+		echo ""
+		echo -e "${textcolor}Sing-Box is installed${clear}"
+		echo ""
+		echo -e "It can be updated with ${textcolor}apt-get install sing-box${clear} command"
+		echo ""
+		main_menu
+	fi
+}
+
+exit_install() {
+	if [[ "$sbinstall" == "x" ]] || [[ "$sbinstall" == "х" ]]
+	then
+		echo ""
+		exit 0
+	fi
+}
+
+exit_add_proxy() {
+	if [[ "$link" == "x" ]] || [[ "$link" == "х" ]]
+	then
+		link=""
+		main_menu
+	fi
+}
+
+exit_del_proxy() {
+	if [[ "$delcomm" == "x" ]] || [[ "$delcomm" == "х" ]]
+	then
+		delcomm=""
+		main_menu
+	fi
+}
+
+check_link() {
+	while [[ -z $link ]] || [[ "$(curl -s -o /dev/null -w "%{http_code}" ${link})" == "000" ]]
+	do
+		if [[ -z $link ]]
+		then
+			:
+		else
+			echo -e "${red}Error: the link is incorrect or the server is not available${clear}"
+			echo ""
+		fi
+		echo -e "${textcolor}[?]${clear} Enter your client config link or enter ${textcolor}x${clear} to exit:"
+		read link
+		[[ ! -z $link ]] && echo ""
+		exit_add_proxy
+	done
+}
+
+check_command_add() {
+	while [[ -f /usr/local/bin/${newcomm} ]] || [[ $newcomm =~ " " ]] || [[ $newcomm =~ '$' ]] || [[ -z $newcomm ]]
+	do
+		if [[ -f /usr/local/bin/${newcomm} ]]
+		then
+			echo -e "${red}Error: this command already exists in /usr/local/bin${clear}"
+			echo ""
+		elif [[ $newcomm =~ " " ]] || [[ $newcomm =~ '$' ]]
+		then
+			echo -e "${red}Error: the command should not contain spaces and \$${clear}"
+			echo ""
+		elif [[ -z $newcomm ]]
+		then
+			:
+		fi
+		echo -e "${textcolor}[?]${clear} Enter the command for the new proxy:"
+		read newcomm
+		[[ ! -z $newcomm ]] && echo ""
+	done
+}
+
+check_command_del() {
+	while [[ -z $delcomm ]] || [[ ! -f /usr/local/bin/${delcomm} ]]
+	do
+		if [[ -z $delcomm ]]
+		then
+			:
+		else
+			echo -e "${red}Error: this command does not exist in /usr/local/bin${clear}"
+			echo ""
+		fi
+		echo -e "${textcolor}[?]${clear} Enter the proxy command you want to delete or enter ${textcolor}x${clear} to exit:"
+		read delcomm
+		[[ ! -z $delcomm ]] && echo ""
+		exit_del_proxy
+	done
+}
+
+show_proxies() {
+	proxynum=$(cat /usr/local/bin/proxylist | wc -l)
+	echo -e "${textcolor}Number of proxies:${clear} ${proxynum}"
+	cat /usr/local/bin/proxylist | sed "s/#//g"
+	echo ""
+	main_menu
+}
+
+add_proxies() {
+	while [[ $link != "x" ]] && [[ $link != "х" ]]
+	do
+		echo -e "${textcolor}[?]${clear} Enter your client config link or enter ${textcolor}x${clear} to exit:"
+		read link
+		[[ ! -z $link ]] && echo ""
+		exit_add_proxy
+		check_link
+		echo -e "${textcolor}[?]${clear} Enter the command for the new proxy:"
+		read newcomm
+		[[ ! -z $newcomm ]] && echo ""
+		check_command_add
+
+		touch /usr/local/bin/${newcomm}
+		cat > /usr/local/bin/${newcomm} <<-EOF
+		#!/bin/bash
+		textcolor='\033[1;34m'
+		red='\033[1;31m'
+		clear='\033[0m'
+		if [[ \$EUID -ne 0 ]]
+		then
+		    echo ""
+		    echo -e "\${red}Error: this command should be run as root, use sudo -i command\${clear}"
+		    echo ""
+		    exit 1
+		fi
+		wget -q -O /etc/sing-box/config.json ${link}
+		systemctl start sing-box.service
+		echo ""
+		echo -e "\${textcolor}Started Sing-Box\${clear}"
+		echo "Do not close this window while Sing-Box is running"
+		echo -e "Enter \${textcolor}x\${clear} to disconnect:"
+		while [[ \$run != "x" ]] && [[ \$run != "х" ]]
+		do
+		    read run
+		done
+		run=""
+		echo ""
+		systemctl stop sing-box.service
+		EOF
+		chmod +x /usr/local/bin/${newcomm}
+		echo "#${newcomm}" >> /usr/local/bin/proxylist
+
+		echo -e "Command ${textcolor}${newcomm}${clear} has been added, use it to connect to the proxy"
+		echo ""
+	done
+
+	main_menu
+}
+
+delete_proxies() {
+	while [[ $delcomm != "x" ]] && [[ $delcomm != "х" ]]
+	do
+		echo -e "${textcolor}[?]${clear} Enter the proxy command you want to delete or enter ${textcolor}x${clear} to exit:"
+		read delcomm
+		[[ ! -z $delcomm ]] && echo ""
+		exit_del_proxy
+		check_command_del
+
+		rm /usr/local/bin/${delcomm}
+		sed -i "/#$delcomm/d" /usr/local/bin/proxylist
+		echo -e "Command ${textcolor}${delcomm}${clear} has been deleted"
+		echo ""
+	done
+
+	main_menu
+}
+
+main_menu() {
+	echo ""
+	echo -e "${textcolor}Select an option:${clear}"
+	echo "0 - Exit"
+    echo "1 - Show the list of proxies"
+    echo "2 - Add a new proxy"
+    echo "3 - Delete a proxy"
+	read option
+	echo ""
+
+	case $option in
+		1)
+		show_proxies
+		;;
+		2)
+		add_proxies
+		;;
+		3)
+		delete_proxies
+		;;
+		*)
+		exit 0
+	esac
+}
+
+check_root
+install_sing_box
+main_menu
--- Scripts/sb-pc-linux-ru.sh
+++ Scripts/sb-pc-linux-ru.sh
@@ -8,224 +8,224 @@ check_root() {
 	if [[ $EUID -ne 0 ]]
 	then
 		echo ""
-		echo -e "${red}Error: this script should be run as root, use \"sudo -i\" command${clear}"
+		echo -e "${red}Ошибка: этот скрипт нужно запускать от имени root, сначала введите команду \"sudo -i\"${clear}"
 		echo ""
 		exit 1
 	fi
 }
 
-enter_language() {
-	echo ""
-	echo -e "${textcolor}Select the language:${clear}"
-	echo "1 - Russian"
-	echo "2 - English"
-	read language
-	echo ""
+install_sing_box() {
+	if [[ ! -f /usr/local/bin/proxylist ]]
+	then
+		touch /usr/local/bin/proxylist
+	fi
+
+	if [ $(sing-box version &> /dev/null; echo $?) -ne 0 ]
+	then
+		echo ""
+		echo -e "${textcolor}Sing-Box не установлен${clear}"
+		echo ""
+		echo -e "${textcolor}[?]${clear} Нажмите ${textcolor}Enter${clear}, чтобы установить, или введите ${textcolor}x${clear}, чтобы выйти:"
+		read sbinstall
+		exit_install
+
+		echo -e "${textcolor}Установка Sing-Box...${clear}"
+		curl -fsSL https://sing-box.app/gpg.key -o /etc/apt/keyrings/sagernet.asc
+		chmod a+r /etc/apt/keyrings/sagernet.asc
+		echo "deb [arch=`dpkg --print-architecture` signed-by=/etc/apt/keyrings/sagernet.asc] https://deb.sagernet.org/ * *" | tee /etc/apt/sources.list.d/sagernet.list > /dev/null
+		apt-get update
+		apt-get install sing-box -y
+		systemctl disable sing-box.service
+
+		echo ""
+		echo -e "${textcolor}Sing-Box установлен${clear}"
+		echo ""
+		echo -e "Его можно обновлять командой ${textcolor}apt-get install sing-box${clear}"
+		echo ""
+		main_menu
+	fi
 }
 
-enter_data_ru() {
-	while [[ -z $link ]]
-	do
-		echo -e "${textcolor}[?]${clear} Введите ссылку на ваш клиентский конфиг:"
-		read link
+exit_install() {
+	if [[ "$sbinstall" == "x" ]] || [[ "$sbinstall" == "х" ]]
+	then
 		echo ""
-	done
-	echo -e "${textcolor}[?]${clear} Введите новую команду для этого прокси (и запомните её):"
-	read newcomm
-	echo ""
-	while [ -f /usr/local/bin/${newcomm} ] || [[ -z $newcomm ]]
+		exit 0
+	fi
+}
+
+exit_add_proxy() {
+	if [[ "$link" == "x" ]] || [[ "$link" == "х" ]]
+	then
+		link=""
+		main_menu
+	fi
+}
+
+exit_del_proxy() {
+	if [[ "$delcomm" == "x" ]] || [[ "$delcomm" == "х" ]]
+	then
+		delcomm=""
+		main_menu
+	fi
+}
+
+check_link() {
+	while [[ -z $link ]] || [[ "$(curl -s -o /dev/null -w "%{http_code}" ${link})" == "000" ]]
 	do
-		if [ -f /usr/local/bin/${newcomm} ]
+		if [[ -z $link ]]
+		then
+			:
+		else
+			echo -e "${red}Ошибка: ссылка введена неправильно или сервер недоступен${clear}"
+			echo ""
+		fi
+		echo -e "${textcolor}[?]${clear} Введите ссылку на ваш клиентский конфиг или введите ${textcolor}x${clear}, чтобы выйти:"
+		read link
+		[[ ! -z $link ]] && echo ""
+		exit_add_proxy
+	done
+}
+
+check_command_add() {
+	while [[ -f /usr/local/bin/${newcomm} ]] || [[ $newcomm =~ " " ]] || [[ $newcomm =~ '$' ]] || [[ -z $newcomm ]]
+	do
+		if [[ -f /usr/local/bin/${newcomm} ]]
 		then
 			echo -e "${red}Ошибка: эта команда уже существует в /usr/local/bin${clear}"
 			echo ""
-		elif [[ -z $newcomm ]]
+		elif [[ $newcomm =~ " " ]] || [[ $newcomm =~ '$' ]]
 		then
-			:
-		fi
-		echo -e "${textcolor}[?]${clear} Введите новую команду для этого прокси (и запомните её):"
-		read newcomm
-		echo ""
-	done
-}
-
-create_sbupdate_ru() {
-	if [ ! -d /etc/sing-box ]
-	then
-		echo "Установка Sing-Box..."
-		echo ""
-		touch /usr/local/bin/sbupdate
-		cat > /usr/local/bin/sbupdate <<-EOF
-		#!/bin/bash
-		red='\033[1;31m'
-		clear='\033[0m'
-		if [[ \$EUID -ne 0 ]]
-		then
-		    echo ""
-		    echo -e "\${red}Ошибка: эту команду нужно запускать от имени root\${clear}"
-		    echo ""
-		    exit 1
-		fi
-		curl -fsSL https://sing-box.app/gpg.key -o /etc/apt/keyrings/sagernet.asc
-		chmod a+r /etc/apt/keyrings/sagernet.asc
-		echo "deb [arch=\`dpkg --print-architecture\` signed-by=/etc/apt/keyrings/sagernet.asc] https://deb.sagernet.org/ * *" | tee /etc/apt/sources.list.d/sagernet.list > /dev/null
-		apt-get update
-		apt-get install sing-box -y
-		systemctl disable sing-box.service
-		EOF
-		chmod +x /usr/local/bin/sbupdate
-		sbupdate
-		echo ""
-		echo "Sing-Box установлен"
-		echo ""
-	fi
-}
-
-create_proxy_command_ru() {
-	touch /usr/local/bin/${newcomm}
-	cat > /usr/local/bin/${newcomm} <<-EOF
-	#!/bin/bash
-	textcolor='\033[1;36m'
-	red='\033[1;31m'
-	clear='\033[0m'
-	if [[ \$EUID -ne 0 ]]
-	then
-	    echo ""
-	    echo -e "\${red}Ошибка: эту команду нужно запускать от имени root\${clear}"
-	    echo ""
-	    exit 1
-	fi
-	wget -q -O /etc/sing-box/config.json ${link}
-	systemctl start sing-box.service
-	echo ""
-	echo "Sing-Box запущен"
-	echo "Не закрывайте это окно, пока Sing-Box работает"
-	echo -e "Введите \${textcolor}x\${clear}, чтобы отключиться:"
-	while [[ \$run != "x" ]] && [[ \$run != "х" ]]
-	do
-	    read run
-	done
-	run=""
-	echo ""
-	systemctl stop sing-box.service
-	EOF
-	chmod +x /usr/local/bin/${newcomm}
-}
-
-message_ru() {
-	echo -e "Вы можете использовать команду ${textcolor}${newcomm}${clear} для подключения к прокси и команду ${textcolor}sbupdate${clear} для обновления Sing-Box"
-	echo ""
-	echo "Если вы хотите добавить больше клиентских конфигов, то запустите этот скрипт ещё раз"
-	echo ""
-}
-
-enter_data_en() {
-	while [[ -z $link ]]
-	do
-		echo -e "${textcolor}[?]${clear} Enter your client config link:"
-		read link
-		echo ""
-	done
-	echo -e "${textcolor}[?]${clear} Enter the new command for this proxy (and remember it):"
-	read newcomm
-	echo ""
-	while [ -f /usr/local/bin/${newcomm} ] || [[ -z $newcomm ]]
-	do
-		if [ -f /usr/local/bin/${newcomm} ]
-		then
-			echo -e "${red}Error: this command already exists in /usr/local/bin${clear}"
+			echo -e "${red}Ошибка: команда не должна содержать пробелы и \$${clear}"
 			echo ""
 		elif [[ -z $newcomm ]]
 		then
 			:
 		fi
-		echo -e "${textcolor}[?]${clear} Enter the new command for this proxy (and remember it):"
+		echo -e "${textcolor}[?]${clear} Введите команду для нового прокси:"
 		read newcomm
-		echo ""
+		[[ ! -z $newcomm ]] && echo ""
 	done
 }
 
-create_sbupdate_en() {
-	if [ ! -d /etc/sing-box ]
-	then
-		echo "Installing Sing-Box..."
-		echo ""
-		touch /usr/local/bin/sbupdate
-		cat > /usr/local/bin/sbupdate <<-EOF
+check_command_del() {
+	while [[ -z $delcomm ]] || [[ ! -f /usr/local/bin/${delcomm} ]]
+	do
+		if [[ -z $delcomm ]]
+		then
+			:
+		else
+			echo -e "${red}Ошибка: эта команда не существует в /usr/local/bin${clear}"
+			echo ""
+		fi
+		echo -e "${textcolor}[?]${clear} Введите удаляемую команду для прокси или введите ${textcolor}x${clear}, чтобы выйти:"
+		read delcomm
+		[[ ! -z $delcomm ]] && echo ""
+		exit_del_proxy
+	done
+}
+
+show_proxies() {
+	proxynum=$(cat /usr/local/bin/proxylist | wc -l)
+	echo -e "${textcolor}Количество прокси:${clear} ${proxynum}"
+	cat /usr/local/bin/proxylist | sed "s/#//g"
+	echo ""
+	main_menu
+}
+
+add_proxies() {
+	while [[ $link != "x" ]] && [[ $link != "х" ]]
+	do
+		echo -e "${textcolor}[?]${clear} Введите ссылку на ваш клиентский конфиг или введите ${textcolor}x${clear}, чтобы выйти:"
+		read link
+		[[ ! -z $link ]] && echo ""
+		exit_add_proxy
+		check_link
+		echo -e "${textcolor}[?]${clear} Введите команду для нового прокси:"
+		read newcomm
+		[[ ! -z $newcomm ]] && echo ""
+		check_command_add
+
+		touch /usr/local/bin/${newcomm}
+		cat > /usr/local/bin/${newcomm} <<-EOF
 		#!/bin/bash
+		textcolor='\033[1;34m'
 		red='\033[1;31m'
 		clear='\033[0m'
 		if [[ \$EUID -ne 0 ]]
 		then
 		    echo ""
-		    echo -e "\${red}Error: this command should be run as root\${clear}"
+		    echo -e "\${red}Ошибка: эту команду нужно запускать от имени root, сначала введите команду sudo -i\${clear}"
 		    echo ""
 		    exit 1
 		fi
-		curl -fsSL https://sing-box.app/gpg.key -o /etc/apt/keyrings/sagernet.asc
-		chmod a+r /etc/apt/keyrings/sagernet.asc
-		echo "deb [arch=\`dpkg --print-architecture\` signed-by=/etc/apt/keyrings/sagernet.asc] https://deb.sagernet.org/ * *" | tee /etc/apt/sources.list.d/sagernet.list > /dev/null
-		apt-get update
-		apt-get install sing-box -y
-		systemctl disable sing-box.service
+		wget -q -O /etc/sing-box/config.json ${link}
+		systemctl start sing-box.service
+		echo ""
+		echo -e "\${textcolor}Sing-Box запущен\${clear}"
+		echo "Не закрывайте это окно, пока Sing-Box работает"
+		echo -e "Введите \${textcolor}x\${clear}, чтобы отключиться:"
+		while [[ \$run != "x" ]] && [[ \$run != "х" ]]
+		do
+		    read run
+		done
+		run=""
+		echo ""
+		systemctl stop sing-box.service
 		EOF
-		chmod +x /usr/local/bin/sbupdate
-		sbupdate
-		echo ""
-		echo "Sing-Box is installed"
-		echo ""
-	fi
-}
+		chmod +x /usr/local/bin/${newcomm}
+		echo "#${newcomm}" >> /usr/local/bin/proxylist
 
-create_proxy_command_en() {
-	touch /usr/local/bin/${newcomm}
-	cat > /usr/local/bin/${newcomm} <<-EOF
-	#!/bin/bash
-	textcolor='\033[1;36m'
-	red='\033[1;31m'
-	clear='\033[0m'
-	if [[ \$EUID -ne 0 ]]
-	then
-	    echo ""
-	    echo -e "\${red}Error: this command should be run as root\${clear}"
-	    echo ""
-	    exit 1
-	fi
-	wget -q -O /etc/sing-box/config.json ${link}
-	systemctl start sing-box.service
-	echo ""
-	echo "Started Sing-Box"
-	echo "Do not close this window while Sing-Box is running"
-	echo -e "Enter \${textcolor}x\${clear} to disconnect:"
-	while [[ \$run != "x" ]] && [[ \$run != "х" ]]
-	do
-	    read run
+		echo -e "Команда ${textcolor}${newcomm}${clear} добавлена, используйте её для подключения к прокси"
+		echo ""
 	done
-	run=""
-	echo ""
-	systemctl stop sing-box.service
-	EOF
-	chmod +x /usr/local/bin/${newcomm}
+
+	main_menu
 }
 
-message_en() {
-	echo -e "You can use ${textcolor}${newcomm}${clear} command to connect to the proxy and ${textcolor}sbupdate${clear} command to update Sing-Box"
+delete_proxies() {
+	while [[ $delcomm != "x" ]] && [[ $delcomm != "х" ]]
+	do
+		echo -e "${textcolor}[?]${clear} Введите удаляемую команду для прокси или введите ${textcolor}x${clear}, чтобы выйти:"
+		read delcomm
+		[[ ! -z $delcomm ]] && echo ""
+		exit_del_proxy
+		check_command_del
+
+		rm /usr/local/bin/${delcomm}
+		sed -i "/#$delcomm/d" /usr/local/bin/proxylist
+		echo -e "Команда ${textcolor}${delcomm}${clear} удалена"
+		echo ""
+	done
+
+	main_menu
+}
+
+main_menu() {
 	echo ""
-	echo "Run this script again if you want to add more client configs"
+	echo -e "${textcolor}Выберите действие:${clear}"
+	echo "0 - Выйти"
+	echo "1 - Вывести список прокси"
+	echo "2 - Добавить новый прокси"
+	echo "3 - Удалить прокси"
+	read option
 	echo ""
+
+	case $option in
+		1)
+		show_proxies
+		;;
+		2)
+		add_proxies
+		;;
+		3)
+		delete_proxies
+		;;
+		*)
+		exit 0
+	esac
 }
 
 check_root
-enter_language
-if [[ "$language" == "1" ]]
-then
-	enter_data_ru
-	create_sbupdate_ru
-	create_proxy_command_ru
-	message_ru
-else
-	enter_data_en
-	create_sbupdate_en
-	create_proxy_command_en
-	message_en
-fi
+install_sing_box
+main_menu
